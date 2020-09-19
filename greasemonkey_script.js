13185d68b531a80898af24ef1592d51268e011d1
Andrey Deriabin <aderiabin.98@gmail.com>
2020-09-19 16:17:37 +0300

    No need to refresh page for each video anymore

--- greasemonkey_script.js
+++ greasemonkey_script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Openedu.ru video speed
-// @version  1
+// @version  2
 // @grant    none
 // @match    https://courses.openedu.ru/*
 // @author   Andrej Deriabin
@@ -8,32 +8,45 @@
 
 // ================================
 // Usage:
-// 		1. Install Greasemonkey browser plugin
+// 		1. Install Greasemonkey plugin for your browser
 //		2. Click 'New user script' option
 //		3. Copy-paste this script. Save it.
-//		4. Enjoy!
-//
-//	Might sometimes require page refresh to work.
-//
-//	If you don't want Greasemonkey plugin, just copy add_speed_option function and call it manually in browser console.
-//	Greasemonkey does this for you each time you (re)load page.
+//		4. Refresh the page
+//		5. Enjoy!
 //
 //=================================
 
 
 {
+  function get_speed_el() {
+     return document.getElementsByClassName('video-speeds')[0];
+  }
+  
   function add_speed_option(speed) {
-		  const el_first = document.getElementsByClassName('video-speeds')[0].children[0];
+      const speed_el = get_speed_el();
+      if (!speed_el)
+      	  return;
+	  const el_first = speed_el.children[0];
       const el_cpy = el_first.cloneNode(true);
       el_cpy.dataset['speed'] = speed;
       el_cpy.children[0].textContent = speed + 'x';
       el_first.before(el_cpy);
   }
   
+  function is_missing_options() {
+    	const speed_el = get_speed_el();
+    	if (!speed_el)
+        	return false;
+   		return speed_el.children.length <= 4;
+  }
+  
   window.onload = () => {
-    	setTimeout(() => {
-      	add_speed_option(2.0);
+    	setInterval(() => {
+        if (is_missing_options()) {
+      		add_speed_option(2.0);
     		add_speed_option(2.5);
+          	add_speed_option(3.0);
+        }
       }, 1000);
   };
 }
