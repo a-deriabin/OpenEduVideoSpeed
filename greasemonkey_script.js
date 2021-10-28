71589ee4f06666784a2f77b99d4a92124358454c
Andrey Deriabin <aderiabin.98@gmail.com>
2021-10-28 15:10:46 +0300

    Fixed script formatting

--- greasemonkey_script.js
+++ greasemonkey_script.js
@@ -15,38 +15,36 @@
 //		5. Enjoy!
 //
 //=================================
-
-
 {
-  function get_speed_el() {
-     return document.getElementsByClassName('video-speeds')[0];
-  }
-  
-  function add_speed_option(speed) {
-      const speed_el = get_speed_el();
-      if (!speed_el)
-      	  return;
-	  const el_first = speed_el.children[0];
-      const el_cpy = el_first.cloneNode(true);
-      el_cpy.dataset['speed'] = speed;
-      el_cpy.children[0].textContent = speed + 'x';
-      el_first.before(el_cpy);
-  }
-  
-  function is_missing_options() {
-    	const speed_el = get_speed_el();
-    	if (!speed_el)
-        	return false;
-   		return speed_el.children.length <= 4;
-  }
-  
-  window.onload = () => {
-    	setInterval(() => {
-        if (is_missing_options()) {
-      		add_speed_option(2.0);
-    		add_speed_option(2.5);
-          	add_speed_option(3.0);
-        }
-      }, 1000);
-  };
+    function get_speed_el() {
+        return document.getElementsByClassName('video-speeds')[0];
+    }
+
+    function add_speed_option(speed) {
+        const speed_el = get_speed_el();
+        if (!speed_el)
+            return;
+        const el_first = speed_el.children[0];
+        const el_cpy = el_first.cloneNode(true);
+        el_cpy.dataset['speed'] = speed;
+        el_cpy.children[0].textContent = speed + 'x';
+        el_first.before(el_cpy);
+    }
+
+    function is_missing_options() {
+        const speed_el = get_speed_el();
+        if (!speed_el)
+            return false;
+        return speed_el.children.length <= 4;
+    }
+
+    window.onload = () => {
+        setInterval(() => {
+            if (is_missing_options()) {
+                add_speed_option(2.0);
+                add_speed_option(2.5);
+                add_speed_option(3.0);
+            }
+        }, 1000);
+    };
 }
